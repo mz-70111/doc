cb4500dc6e2335f2ecbb161197bb5f4f30b7e426
mz-70111 <111832494+mz-70111@users.noreply.github.com>
2023-11-08 20:52:57 +0300

    2

--- python/example.py
+++ python/example.py
@@ -1,14 +1,5 @@
-mylist = [1, 5, 4, 9]
-mylist2 = ["d", "ddd"]
-mylist3 = ['k', 'g', 6]
-mystring = "hellow from me"
-mylist.extend(mylist3)
-mylist.extend(mylist3)
-print(mylist)
+seta={1,2,3}
+setb={1,4,5}
 
-z = ("A", "B", "C")
-a, b, c = z
-print(b)  # B
-x = ("D", "E", 4, "G")
-a, _, b, c = x
-print(c)  # 4
+seta-seta
+print(seta)
